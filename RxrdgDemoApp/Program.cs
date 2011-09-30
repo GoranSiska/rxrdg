ec331e79cf484346724731c995298fa5cf746e5f
Goran Siska <goran.siska@integrum.si,goran.siska@gmail.com>
2011-09-30 22:08:12 +0200

    Reorganization; DemoApp

---
+++ RxrdgDemoApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RxrdgDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter regular expression to generate random sequence or leave empty to quit:");
+                var param = Console.ReadLine();
+
+                if(string.IsNullOrEmpty(param)) { return; }
+
+                try
+                {
+                    var rxrdg = new RegularExpressionDataGenerator.RegExpDataGenerator(param);
+
+                    //show tree
+                    Console.WriteLine();
+                    Console.WriteLine(RegularExpressionDataGenerator.XmlVisitor.Visit(new RegularExpressionDataGenerator.Parser().Parse(param)));
+                    Console.WriteLine("-------");
+                    Console.WriteLine();
+
+                    //output 10 examples
+                    for (var i = 0; i < 10; i++)
+                    {
+                        Console.WriteLine(rxrdg.Next());
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Regular expression sequence not recognized.");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
