e7dc8515d51bda575e6fd99cd2bc92cfeb56453f
Goran Siska <goran.siska@integrum.si,goran.siska@gmail.com>
2011-10-02 10:05:31 +0200

    Tokens as structs

--- RegularExpressionRandomDataGenerator/Lexer.Tokens.cs
+++ RegularExpressionRandomDataGenerator/Lexer.Tokens.cs
@@ -1,90 +1,90 @@
 ﻿
 namespace RegularExpressionDataGenerator
 {
-    public class LiteralToken : IToken
+    public struct LiteralToken : IToken
     {
         public char Character { get; set; }
         public TokenType TokenType { get { return TokenType.Literal; } }
     }
 
-    public class RepetitionToken : IToken
+    public struct RepetitionToken : IToken
     {
         public int MinOccurs { get; set; }
         public int MaxOccurs { get; set; }
         public TokenType TokenType { get { return TokenType.Repetition; } }
     }
 
-    public class ParenthesisLeftToken : IToken
+    public struct ParenthesisLeftToken : IToken
     {
         public TokenType TokenType { get { return TokenType.ParenthesisLeft; } }
     }
 
-    public class ParenthesisRightToken : IToken
+    public struct ParenthesisRightToken : IToken
     {
         public TokenType TokenType { get { return TokenType.ParenthesisRight; } }
     }
 
-    public class ConcatenationToken : IToken
+    public struct ConcatenationToken : IToken
     {
         public TokenType TokenType { get { return TokenType.Concatenation; } }
     }
 
-    public class AlternationToken : IToken
+    public struct AlternationToken : IToken
     {
         public TokenType TokenType { get { return TokenType.Alternation; } }
     }
 
-    public class BracketLeftToken : IToken 
+    public struct BracketLeftToken : IToken 
     {
         public TokenType TokenType { get { return TokenType.BracketLeft; } }
     }
 
-    public class BracketRightToken : IToken 
+    public struct BracketRightToken : IToken 
     {
         public TokenType TokenType { get { return TokenType.BracketRight; } }
     }
 
-    public class RangeToken : IToken
+    public struct RangeToken : IToken
     {
         public TokenType TokenType { get { return TokenType.Range; } }
     }
 
-    public class NotToken : IToken
+    public struct NotToken : IToken
     {
         public TokenType TokenType { get { return TokenType.Not; } }
     }
 
-    public class AnyToken : IToken
+    public struct AnyToken : IToken
     {
         public TokenType TokenType { get { return TokenType.Any; } }
     }
 
-    public class NumericToken : IToken
+    public struct NumericToken : IToken
     {
         public TokenType TokenType { get { return TokenType.Numeric; } }
     }
 
-    public class WordToken : IToken
+    public struct WordToken : IToken
     {
         public TokenType TokenType { get { return TokenType.Word; } }
     }
 
-    public class WhitespaceToken : IToken
+    public struct WhitespaceToken : IToken
     {
         public TokenType TokenType { get { return TokenType.Whitespace; } }
     }
 
-    public class NonNumericToken : IToken
+    public struct NonNumericToken : IToken
     {
         public TokenType TokenType { get { return TokenType.NonNumeric; } }
     }
 
-    public class NonWordToken : IToken
+    public struct NonWordToken : IToken
     {
         public TokenType TokenType { get { return TokenType.NonWord; } }
     }
 
-    public class NonWhitespaceToken : IToken
+    public struct NonWhitespaceToken : IToken
     {
         public TokenType TokenType { get { return TokenType.NonWhitespace; } }
     }
